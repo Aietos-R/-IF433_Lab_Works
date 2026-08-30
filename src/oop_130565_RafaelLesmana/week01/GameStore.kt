5a4f57c326206d7e23f16413eb6079903dc21e3b
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-08-30 15:20:44 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_130565_RafaelLesmana/week01/GameStore.kt
+++ src/oop_130565_RafaelLesmana/week01/GameStore.kt
@@ -3,4 +3,14 @@ package oop_130565_RafaelLesmana.week01
 fun main() {
     val gameTitle = "Resident Evil Requiem"
     val price = 900000.0
+
+    calculateDiscount(price)
+}
+
+fun calculateDiscount(price: Double) : Double {
+    if (price < 500000.0) {
+        return price - (price * 0.2)
+    }else {
+        return price - (price * 0.1)
+    }
 }
