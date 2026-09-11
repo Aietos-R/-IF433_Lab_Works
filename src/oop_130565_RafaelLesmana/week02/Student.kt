33b395203f41d16e112c4bd2f32a228c8dda8d81
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-09-11 22:15:05 +0700

    week02: add warning validation in init block

--- src/oop_130565_RafaelLesmana/week02/Student.kt
+++ src/oop_130565_RafaelLesmana/week02/Student.kt
@@ -5,6 +5,14 @@ class Student( new*
     val nim: String,
     var major: String
 ) {
-
+    init {
+        new *
+                if (nim.length != 5) {
+                    println("Warning : Objek tercipta dengan NIM ($nim) yang tidak valid !")
+                    println("Data mahasiswa $name mungkin akan bermasalah di sistem.")
+                } else {
+                    println("Log : Objek student $name berhasil dialokasikan di Memory.")
+                }
+    }
 }
 
