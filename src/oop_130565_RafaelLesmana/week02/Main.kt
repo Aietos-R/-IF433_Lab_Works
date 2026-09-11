8758a7f6acee57157b108aa93f8812263956eaa4
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-09-11 22:23:09 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_130565_RafaelLesmana/week02/Main.kt
@@ -0,0 +1,23 @@
+package oop_130565_RafaelLesmana.week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.'in')
+    println(" --- Aplikasi PMB UMN --- ")
+    println("Masukkan Nama : ")
+    val name = scanner.nextline()
+    println("Masukkan NIM (Wajib5 Karakter) : ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.length != 5){
+        println("Error : Pendaftaran dibatalkan. NIM harus 5 karakter !")
+    } else {
+        println("Masukkan Jurusan : ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Status : Pendaftaran Selesai.")
+    }
+}
