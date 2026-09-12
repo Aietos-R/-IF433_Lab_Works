fdf0357d52e44e16864a7919fbf260fb5b8b99d9
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-09-12 10:22:43 +0700

    week03: add private property

--- src/oop_130565_RafaelLesmana/week03/Employee.kt
+++ src/oop_130565_RafaelLesmana/week03/Employee.kt
@@ -12,4 +12,14 @@ class Employee (
                 field = value
             }
         }
+    private var performanceRating: Int = 3
+
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat ! Rating $performanceRating")
+    }
+
+    fun printStatus() {
+        println("Karyawan : $name, Rating : $performanceRating")
+    }
 }
