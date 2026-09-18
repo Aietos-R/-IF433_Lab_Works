f33de3bd891690741976aebf2de4f9201653ae0e
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-09-18 22:23:15 +0700

    week03: (task 1) create weapon class with validation

---
+++ src/oop_130565_RafaelLesmana/week03/Toko_Senjata/Weapon.kt
@@ -0,0 +1,23 @@
+package oop_130565_RafaelLesmana.week03.Toko_Senjata
+
+class Weapon (
+    val name : String,
+) {
+    var damage: Int = 0
+        set(value) {
+            if (value < 0) {
+                println("Damage tidak boleh negatif!")
+            } else if (value > 1000) { println("Damage tidak boleh lebih dari 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
+}
