8d7e03db720a520bf8924c3a688c0362a4e2e202
Rafael Lesmana <rafael.lesmana@student.umn.ac.id>
2026-09-18 22:32:32 +0700

    week03: (task 1) create player class with private xp

---
+++ src/oop_130565_RafaelLesmana/week03/Toko_Senjata/Player.kt
@@ -0,0 +1,19 @@
+package oop_130565_RafaelLesmana.week03.Toko_Senjata
+
+class Player(val username: String) {
+    private var xp: Int = 0
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val previousLevel = level
+            xp += amount
+            if (level > previousLevel) {
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        } else {
+            println("XP harus angka positif!")
+        }
+    }
+}
